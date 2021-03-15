a81bd6823115f1f33e0d28e1f27dae44495ebf25
Raymond Lynch <daemitus@gmail.com>
2021-03-15 18:36:14 -0400

    RetainerTaskResult

---
+++ ClickLib/Clicks/ClickRetainerTaskResult.cs
@@ -0,0 +1,17 @@
+﻿using Dalamud.Plugin;
+using FFXIVClientStructs.FFXIV.Client.UI;
+
+namespace ClickLib.Clicks
+{
+    internal sealed class ClickRetainerTaskResult : ClickBase
+    {
+        protected override string Name => "RetainerTaskResult";
+        protected override string AddonName => "RetainerTaskResult";
+
+        public unsafe ClickRetainerTaskResult(DalamudPluginInterface pluginInterface) : base(pluginInterface)
+        {
+            AvailableClicks["retainer_venture_confirm"] = (addon) => SendClick(addon, EventType.CHANGE, 2, ((AddonRetainerTaskResult*)addon)->ConfirmButton->AtkComponentBase.OwnerNode);
+            AvailableClicks["retainer_venture_reassign"] = (addon) => SendClick(addon, EventType.CHANGE, 3, ((AddonRetainerTaskResult*)addon)->ReassignButton->AtkComponentBase.OwnerNode);
+        }
+    }
+}
