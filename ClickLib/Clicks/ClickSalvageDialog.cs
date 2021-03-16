9e108a2bcda9d2d4cf8f18d330964f505f278d43
Raymond Lynch <daemitus@gmail.com>
2021-03-16 11:56:50 -0400

    Formatting, disable yesno check

--- ClickLib/Clicks/ClickSalvageDialog.cs
+++ ClickLib/Clicks/ClickSalvageDialog.cs
@@ -10,8 +10,8 @@ namespace ClickLib.Clicks
 
         public unsafe ClickSalvageDialog(DalamudPluginInterface pluginInterface) : base(pluginInterface)
         {
-            AvailableClicks["desynthesize_checkbox"] = (addon) => SendClick(addon, EventType.CHANGE, 3, ((AddonSalvageDialog*)addon)->CheckBox->AtkComponentButton.AtkComponentBase.OwnerNode);
             AvailableClicks["desynthesize"] = (addon) => SendClick(addon, EventType.CHANGE, 1, ((AddonSalvageDialog*)addon)->DesynthesizeButton->AtkComponentBase.OwnerNode);
+            AvailableClicks["desynthesize_checkbox"] = (addon) => SendClick(addon, EventType.CHANGE, 3, ((AddonSalvageDialog*)addon)->CheckBox->AtkComponentButton.AtkComponentBase.OwnerNode);
         }
     }
 }
--- ClickLib/Clicks/ClickSelectYesno.cs
+++ ClickLib/Clicks/ClickSelectYesno.cs
@@ -12,7 +12,9 @@ namespace ClickLib.Clicks
         {
             AvailableClicks["select_yes"] = (addon) => SendClick(addon, EventType.CHANGE, 0, ((AddonSelectYesno*)addon)->YesButton->AtkComponentBase.OwnerNode);
             AvailableClicks["select_no"] = (addon) => SendClick(addon, EventType.CHANGE, 1, ((AddonSelectYesno*)addon)->NoButton->AtkComponentBase.OwnerNode);
-            AvailableClicks["select_checkbox"] = (addon) => SendClick(addon, EventType.CHANGE, 3, ((AddonSelectYesno*)addon)->ConfirmCheckBox->AtkComponentButton.AtkComponentBase.OwnerNode);
+            
+            // This doesn't actually work. However toggling the button's ResNode flags (1 << 5) makes it clickable.
+            // AvailableClicks["select_checkbox"] = (addon) => SendClick(addon, EventType.CHANGE, 3, ((AddonSelectYesno*)addon)->ConfirmCheckBox->AtkComponentButton.AtkComponentBase.OwnerNode);
         }
     }
 }
