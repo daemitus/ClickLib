f70175aee47844971ffa7c1f232f2e9803996bc3
Raymond Lynch <daemitus@gmail.com>
2021-03-19 20:41:04 -0400

    AddonGrandCompanySupplyReward

---
+++ ClickLib/Clicks/ClickGrandCompanySupplyReward.cs
@@ -0,0 +1,17 @@
+﻿using Dalamud.Plugin;
+using FFXIVClientStructs.FFXIV.Client.UI;
+
+namespace ClickLib.Clicks
+{
+    internal sealed class ClickGrandCompanySupplyReward : ClickBase
+    {
+        protected override string Name => "GrandCompanySupplyReward";
+        protected override string AddonName => "GrandCompanySupplyReward";
+
+        public unsafe ClickGrandCompanySupplyReward(DalamudPluginInterface pluginInterface) : base(pluginInterface)
+        {
+            AvailableClicks["grand_company_expert_delivery_deliver"] = (addon) => SendClick(addon, EventType.CHANGE, 0, ((AddonGrandCompanySupplyReward*)addon)->DeliverButton->AtkComponentBase.OwnerNode);
+            AvailableClicks["grand_company_expert_delivery_cancel"] = (addon) => SendClick(addon, EventType.CHANGE, 1, ((AddonGrandCompanySupplyReward*)addon)->CancelButton->AtkComponentBase.OwnerNode);
+        }
+    }
+}
