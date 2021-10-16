9a9a1b7a17417b88c29e1a4531acde4cffb3df61
marimelon <29140293+marimelon@users.noreply.github.com>
2021-10-16 22:45:02 +0900

    Add ClickContentsFinderConfirm

---
+++ ClickLib/Clicks/ClickContentsFinderConfirm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using FFXIVClientStructs.FFXIV.Client.UI;
+
+namespace ClickLib.Clicks
+{
+    /// <summary>
+    /// Addon ContentsFinderConfirm.
+    /// </summary>
+    public sealed unsafe class ClickContentsFinderConfirm : ClickBase<AddonContentsFinderConfirm>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClickContentsFinderConfirm"/> class.
+        /// </summary>
+        /// <param name="addon">Addon pointer.</param>
+        public ClickContentsFinderConfirm(IntPtr addon = default)
+            : base(addon)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override string AddonName => "ContentsFinderConfirm";
+
+        /// <summary>
+        /// Click the commence button.
+        /// </summary>
+        [ClickName("contentsfinderconfirm_commence")]
+        public void Collect()
+        {
+            ClickAddonButton(&this.Addon->AtkUnitBase, this.Addon->CommenceButton, 0x7);
+        }
+    }
+}
